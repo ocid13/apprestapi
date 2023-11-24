d854f94b79242811dbc17d681aa916f6eb7d879c
Ocid <haidakhoirurrosyid@gmail.com>
2023-11-24 21:29:11 +0700

    berhasil menampilkan data API

--- controller.js
+++ controller.js
@@ -4,5 +4,16 @@ var response = require('./rest');
 var connection = require('./koneksi');
 
 exports.index = function(req, res) {
-  response.ok('REST API berhasil berjalan!');
+  response.ok('REST API berhasil berjalan!', res);
+};
+
+//menampilkan semau data API
+exports.dataApi = function(req, res) {
+  connection.query('SELECT * FROM culinary', function(error, rows, fields) {
+    if(error) {
+      connection.log(error);
+    } else {
+      response.ok(rows, res)
+    }
+  });
 };
